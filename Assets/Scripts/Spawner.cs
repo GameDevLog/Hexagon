0428ecaf1512934016f2d687b242ed5aa3785ae8
iOSDevLog <iosdevlog@iosdevlog.com>
2019-12-10 18:40:35 +0800

    feat(swawner): prefabs

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float spawnRate = 1f;
+    public GameObject hexoagonPrefab;
+    private float nextTimeToSpawn;
+
+    void Update()
+    {
+        if (Time.time >= nextTimeToSpawn)
+        {
+            Instantiate(hexoagonPrefab, Vector3.zero, Quaternion.identity);
+            nextTimeToSpawn = Time.time + 1f / spawnRate;
+        }
+    }
+}
